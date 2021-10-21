47def8fb667af0afcacfd90723369de1279d57e0
NikkiB101 <nikkitab101@gmail.com>
2021-10-21 13:56:32 +0000

    Add create list item

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,11 +4,21 @@ let enterToDoItem = document.getElementById("add-item");
 let addToDoItem = document.getElementById("add-button");
 let toDoList = document.getElementsByTagName("ul");
 
-//Add To Do Item
+//Functions
 
-function addToDo () {
+//Create Div for item on to do list.
+
+function addToDo (event) {
     let newItemDiv = document.createElement("div");
     newItemDiv.classList.add("item-div");
+
+//Create List item 
+
+    let newToDo = document.createElement("li");
+    newToDo.classList.add("list-item");
+    newToDo.innerText = form-text.value;
+    newItemDiv.appendChild(newToDo);
+
+    event.preventDefault();
 }
 
-addToDo();
