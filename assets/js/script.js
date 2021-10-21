3b14c28e41aea77f483ba7e0869e7444eed013d3
NikkiB101 <nikkitab101@gmail.com>
2021-10-21 13:43:00 +0000

    Add function to create div and assign class when item added

---
+++ assets/js/script.js
@@ -0,0 +1,14 @@
+//Selectors
+
+let enterToDoItem = document.getElementById("add-item");
+let addToDoItem = document.getElementById("add-button");
+let toDoList = document.getElementsByTagName("ul");
+
+//Add To Do Item
+
+function addToDo () {
+    let newItemDiv = document.createElement("div");
+    newItemDiv.classList.add("item-div");
+}
+
+addToDo();
